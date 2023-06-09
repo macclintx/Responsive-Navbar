4fd7f309eb0495205b612a6749cad5164077a4ef
macclintx <macclintx@gmail.com>
2023-06-09 12:27:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var clicks=0;
+var err=0;
+document.addEventListener('click',function(l){
+    let el_id=l.target.id;
+    if(el_id=="menu-icon"){
+        document.querySelector(".menu-small-screen").style.marginTop="34px";
+        document.querySelector(".menu-icon").style.transform="rotate(50deg)";
+        ++clicks;
+
+        if(clicks%2==0){
+            document.querySelector(".menu-small-screen").style.marginTop="-400px";
+            document.querySelector(".menu-icon").style.transform="rotate(0deg)";
+        }
+    }
+})
